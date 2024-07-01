b585294fbc1b2783ce2b538fbea8a117af2852d3
Chukwuemika11 <izunduchukwuemika9@gmail.com>
2024-07-01 23:59:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+print("Welcome to my bank!")
+print("lets create your username and password")
+age = input(int("how old are you?"))
+name = input("what is your name?")
+accountNumber = int(random.randint(0, 11))
+userName = (input("Enter your username"))
+password = (input("Enter your password"))
+
+print(f"your name is: {name} + " " +  your password is: {password} + " " your account number is: {accountNumber} + " " your age is: {age}")
+print("Your welcome to Zempaa bank")
